05c0e9ecd1a571c6fe8ba407d002c165a5773ec4
Jake Romer <mail@jakeromer.com>
2020-04-17 19:51:36 -0400

    feat[ui] Add new fields to campaign detail view

--- services/ui/src/views/CampaignDetailsView/index.js
+++ services/ui/src/views/CampaignDetailsView/index.js
@@ -8,9 +8,12 @@ import {useViewTitle} from '../../contexts/ViewContext';
 
 const GET_CAMPAIGN = gql`
     query ListCampaigns($id: Int!) {
-        campaign(id: $id) @rest(type: "Campaign", path: "/campaigns/{args.id}") {
+        campaign(id: $id)
+            @rest(type: "Campaign", path: "/campaigns/{args.id}") {
             id
             name
+            description
+            isActive
             createdOn
             updatedOn
         }
@@ -19,8 +22,8 @@ const GET_CAMPAIGN = gql`
 
 const CampaignDetailsView = () => {
     useViewTitle('Campaign Details');
-    const { id } = useParams();
-    const { loading, error, data } = useQuery(GET_CAMPAIGN, { variables: { id } });
+    const {id} = useParams();
+    const {loading, error, data} = useQuery(GET_CAMPAIGN, {variables: {id}});
 
     if (loading) {
         return <span>Loading...</span>;
@@ -30,12 +33,19 @@ const CampaignDetailsView = () => {
         return <span>An error occurred. We could not fetch the campaign</span>;
     }
 
-    const campaign = data?.campaign || {};
+    const {name, description, isActive, createdOn} = data?.campaign || {};
 
     return (
         <>
-            <Title>{campaign.name}</Title>
-            <Typography component="p" variant="body1">{campaign.name} was created on {campaign.createdOn}</Typography>
+            <Title>{name}</Title>
+            <Typography component='h2'>{description}</Typography>
+            <Typography component='p' variant='body2'>
+                Campaign <strong>{name}</strong> is{' '}
+                <strong>{isActive ? 'live' : 'not live'}</strong>.
+            </Typography>
+            <Typography component='p' variant='body2'>
+                Created on {createdOn}
+            </Typography>
         </>
     );
 };
